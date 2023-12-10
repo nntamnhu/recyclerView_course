2ec7c3ad6a887f81e0fb2b1ac8a75eef6db12fc6
Nguyen Ngoc Tam Nhu <nntamnhu01@gmail.com>
2023-12-10 17:00:46 +0700

    updatebtnddCourse

--- app/src/main/java/com/example/recyclerview_course/MainActivity.java
+++ app/src/main/java/com/example/recyclerview_course/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private List<Course> courseList;
     Button btnAddCourse;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,24 +30,62 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         courseList = new ArrayList<>();
 
-        courseList.add(new Course("CMP354", "Lập Trình Di Động", "Trịnh Đồng Thạch Trúc", R.mipmap.ic_launcher ));
+        courseList.add(new Course("CMP354", "Lập Trình Di Động", "Trịnh Đồng Thạch Trúc", R.mipmap.ic_launcher));
         courseList.add(new Course("CMP324", "Lập Trình Java", "Nguyễn Thanh Tùng", R.drawable.java));
         courseList.add(new Course("CMP332", "Lập Trình Windows", "Nguyễn Huy Cường", R.drawable.win));
 
-        adapter = new CourseAdapter(this,courseList);
+        adapter = new CourseAdapter(this, courseList);
         recyclerView.setAdapter(adapter);
 
         btnAddCourse = findViewById(R.id.btnAddCourse);
         btnAddCourse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Thêm một khóa học mới vào danh sách
-                courseList.add(new Course("New Course", "NEW123", "New Course", R.drawable.java));
 
-                // Thông báo cho Adapter biết rằng dữ liệu đã thay đổi
-                adapter.notifyDataSetChanged();
+                //openAddCourseDialog();
+
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.add_course);
+
+                EditText edtCourseCode = dialog.findViewById(R.id.edtCourseCode);
+                EditText edtCourseName = dialog.findViewById(R.id.edtCourseName);
+                EditText edtLecturerName = dialog.findViewById(R.id.edtLecturerName);
+
+                Button btnAdd = dialog.findViewById(R.id.btnAdd);
+                btnAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String courseCode = "", courseName = "", lecturerName = "";
+
+                        if (!edtCourseCode.getText().toString().equals("")) {
+                            courseCode = edtCourseCode.getText().toString();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Please enter course code!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (!edtCourseName.getText().toString().equals("")) {
+                            courseName = edtCourseName.getText().toString();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Please enter course name!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (!edtLecturerName.getText().toString().equals("")) {
+                            lecturerName = edtLecturerName.getText().toString();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Please enter lecturer name!", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                        courseList.add(new Course(courseCode, courseName, lecturerName, R.drawable.c));
+                        adapter.notifyItemChanged(courseList.size() - 1);
+                        recyclerView.scrollToPosition(courseList.size() - 1);
+
+                        // Đóng Dialog sau khi thêm xong
+//                dialog.dismiss();
+                    }
+                });
+                dialog.show();
             }
         });
     }
-
 }
